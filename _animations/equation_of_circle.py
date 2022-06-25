e4ed9f7ac56a403f01eba61a2d9acdad389a34bf
abhinandshibu <53155408+abhinandshibu@users.noreply.github.com>
2022-06-25 15:18:00 +0100

    Added animation to rotate a radius to form a circle.

--- _animations/equation_of_circle.py
+++ _animations/equation_of_circle.py
@@ -9,15 +9,18 @@ def fadeOutObjectsInList(self, listOfObjects):
 class DerivationOfEquationOfCircle(Scene):
     def construct(self):
         self.add(NumberPlane())
-        # objects = self.intro()
-        # self.wait(0.5)
-        # fadeOutObjectsInDictionary(self, objects)
-        self.drawCenterAndRadiusOfCircle()
 
+        # introDict = self.intro()
+        # fadeOutObjectsInDictionary(self, introDict)
+
+        centerRadiusDict = self.drawCenterAndRadiusOfCircle()
+        self.rotateRadiusToFormCircle(radius=centerRadiusDict["radius"], centerPosition=centerRadiusDict["centerDot"].get_center())
+    
     def intro(self):
         text = Text("What is the equation \n of a circle?").move_to(RIGHT * 3)
         circle = Circle(color=BLUE).scale(2).move_to(LEFT * 3)
         self.play(Write(text), Write(circle))
+        self.wait(0.5)
         
         return {
             "text": text,
@@ -25,20 +28,56 @@ class DerivationOfEquationOfCircle(Scene):
         }
 
     def drawCenterAndRadiusOfCircle(self):
-        center = Dot(color=RED)
-        radius = VGroup(
-                    Line(start=ORIGIN, end=2*RIGHT).set_angle(45*DEGREES),
-                    Dot(2*RIGHT)
-                )
-        self.play(Write(center), Write(radius))
-        self.play(
-            Rotate(
-                radius,
-                angle=45*DEGREES,
-                about_point=ORIGIN,
-            )
+        centerDot = Dot(color=ORANGE)
+        centerText = Text("Center").scale(0.5).next_to(centerDot, DOWN)
+        
+        self.play(Write(centerDot), Write(centerText))
+        self.wait(0.5)
+
+        radius = Arrow(start=ORIGIN, end=[2,2,0], color=BLUE, buff=0)
+        
+        self.play(Write(radius))
+        self.wait(0.5)
+
+        # self.play(
+        #     Rotate(
+        #         radius,
+        #         angle=360*DEGREES,
+        #         about_point=ORIGIN,
+        #     ),
+        #     run_time=3
+        # )
+        return {
+            "centerDot": centerDot, 
+            "centerText": centerText, 
+            "radius": radius
+        }
+
+    def rotateRadiusToFormCircle(self, radius, centerPosition):
+        angleInDegrees = ValueTracker(0)
+
+        radiusRef = radius.copy()
+        radius.add_updater(
+            lambda z: z.become(radiusRef).rotate(angleInDegrees.get_value() * DEGREES, about_point=centerPosition)
         )
-        return (center, radius)
+
+        circlePath = VMobject()
+        circlePath.set_points_as_corners([radius.get_end(), radius.get_end()])
+        def update_path(path):
+            previous_path = path.copy()
+            previous_path.add_points_as_corners([radius.get_end()])
+            path.become(previous_path)
+        circlePath.add_updater(update_path)
+        
+        self.add(circlePath)
+        self.play(
+            angleInDegrees.animate.set_value(360),
+            run_time=2.5
+        )
+
+        self.wait(0.5)
+
+
 
     
 
